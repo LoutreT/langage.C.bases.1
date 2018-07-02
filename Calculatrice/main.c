6fdaf9131c7358955a678b7f6b3f4ebd1718f401
Vincent <v.nassaux@nootos.com>
2018-07-02 23:48:54 +0200

    Experience %d pour integer 'int' pour 'input'

---
+++ Calculatrice/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int resultat = 0, nombre1 = 0, nombre2 = 0;
+
+    printf("Quel nombre1 voulez-vous additionner ?");
+    scanf("%d", &nombre1);
+    printf("Quel nombre2 voulez-vous additionner ?");
+    scanf("%d", &nombre2);
+
+    resultat = nombre1 + nombre2;
+    printf("%d + %d = %d\n", nombre1, nombre2, resultat);
+
+    // réponse en 4.636 s !
+    // Vincent Nassaux
+
+    return 0;
+}
