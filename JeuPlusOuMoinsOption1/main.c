4f185c0d9e1dd555b4438f8c878a937e513e0844
Vincent <v.nassaux@nootos.com>
2018-07-05 23:59:47 +0200

    a

---
+++ JeuPlusOuMoinsOption1/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    const int MIN = 1;  // Ici j'ai choisi de séparer mes 2 constantes en 2 lignes séparées
+    const int MAX = 100;
+
+    int nombreADeviner = 0;  // Ici j'ai choisi de séparer mes 'int' en plusieurs lignes séparées
+    int nombre = 0;
+    int nombreDEssai = 0;
+    int essai = 0;
+
+    srand(time(NULL));
+    nombreADeviner = (rand() % (MAX - MIN + 1)) + MIN;
+
+    printf("Hello ! Voici le jeu nomme 'Plus Ou Moins'\n\n La regle du jeu est de mettre un \n nombre jusqu'a ce que vous trouviez le bon nombre cache\n");
+
+    do
+    {
+        printf("\n\nVoici, choisissez un nombre entre 0 et 100\n\n");
+        scanf("%d", &nombre);
+
+        if (nombre == nombreADeviner)
+            printf("Voici, c'est le bon nombre !\n ");
+            essai++;
+
+        else if(nombre > nombreADeviner)
+            printf("Non, il faut chercher un nombre plus petit \n");
+            essai++;
+
+        else if(nombre < nombreADeviner)
+            printf("Ce n'est pas ca, il faut chercher un nombre plus grand \n");
+            essai++;
+
+    }while(nombre != nombreADeviner);
+
+    printf("Vous avez joue en %d ", &nombreDEssai);
+
+    nombreDEssai += essai ;
+
+    return 0;
+}
