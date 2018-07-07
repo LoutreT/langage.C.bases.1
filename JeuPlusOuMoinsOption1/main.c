e3b6edd1bce9d14639efbf8ea07b2534791a0bfc
Vincent <v.nassaux@nootos.com>
2018-07-07 12:37:00 +0200

    Phase2

--- JeuPlusOuMoinsOption1/main.c
+++ JeuPlusOuMoinsOption1/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main()
+int main(int argc, char *argv[])
 {
     const int MIN = 1;  // Ici j'ai choisi de séparer mes 2 constantes en 2 lignes séparées
     const int MAX = 100;
@@ -15,30 +15,36 @@ int main()
     srand(time(NULL));
     nombreADeviner = (rand() % (MAX - MIN + 1)) + MIN;
 
+    printf(nombreADeviner);
     printf("Hello ! Voici le jeu nomme 'Plus Ou Moins'\n\n La regle du jeu est de mettre un \n nombre jusqu'a ce que vous trouviez le bon nombre cache\n");
 
     do
     {
-        printf("\n\nVoici, choisissez un nombre entre 0 et 100\n\n");
+        nombreDEssai++;
+        printf("\n\n Voici, choisissez un nombre entre 0 et 100 \n\n");
         scanf("%d", &nombre);
+
 
         if (nombre == nombreADeviner)
             printf("Voici, c'est le bon nombre !\n ");
-            essai++;
 
         else if(nombre > nombreADeviner)
             printf("Non, il faut chercher un nombre plus petit \n");
-            essai++;
 
-        else if(nombre < nombreADeviner)
-            printf("Ce n'est pas ca, il faut chercher un nombre plus grand \n");
-            essai++;
+        else // (nombre < nombreADeviner)
+            printf("Ce n est pas ca il faut chercher un nombre plus grand \n");
+
+
+        // nombreDEssai += essai ;
+        // printf("Vous avez joue en %d ", &nombreDEssai);
+
 
     }while(nombre != nombreADeviner);
 
-    printf("Vous avez joue en %d ", &nombreDEssai);
+    // printf("Vous avez joue en %d ", nombreDEssai);
 
-    nombreDEssai += essai ;
+
 
     return 0;
 }
+
