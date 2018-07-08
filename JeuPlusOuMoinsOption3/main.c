7b075e0f69fffd30798928ea15ce46151706bb9a
Vincent <v.nassaux@nootos.com>
2018-07-08 06:02:57 +0200

    Phase4 inclusion de 2 types de joueurs

--- JeuPlusOuMoinsOption3/main.c
+++ JeuPlusOuMoinsOption3/main.c
@@ -10,7 +10,8 @@ int main(int argc, char *argv[])
     int nombreADeviner = 0;  // Ici j'ai choisi de séparer mes 'int' en plusieurs lignes séparées
     int nombre = 0;
     int nombreDEssai = 0;
-    int continuerPartie = 1;
+    int continuerPartie = 0;
+    int soloOuDuo = 0;
 
     srand(time(NULL));
 
@@ -18,52 +19,59 @@ int main(int argc, char *argv[])
 
     do
     {
-        int soloOuDuo = 0;
-        printf("\n Comment desirez vous le jouer ? \n Vous souhaiteriez jouer en solo ou bien vous \n etes deux personnes qui souhaiteraient faire deviner \n entre elles le bon nombre a deviner ?");
+        printf("\nComment desirez vous le jouer ? \n Vous souhaiteriez jouer en solo ou bien vous \n etes deux personnes qui souhaiteraient faire deviner \n entre elles le bon nombre a deviner ?");
         printf("\n 3 = En solo ");
         printf("\n 4 = A deux \n ");
-        scanf("%d", soloOuDuo);
+        scanf("%d", &soloOuDuo);
 
         if(soloOuDuo == 3)
         {
+            printf("\n\n Voici, choisissez un nombre entre 0 et 100 \n\n");
+
             nombreADeviner = (rand() % (MAX - MIN + 1)) + MIN;
             // printf(nombreADeviner);  // ---> Pour un test, à cause de cette ligne, mon jeu ne fonctionnait plus.
-            printf("\n\n Voici, choisissez un nombre entre 0 et 100 \n\n");
 
             do
             {
                 nombreDEssai++;
-
                 scanf("%d", &nombre);
 
                 if (nombre == nombreADeviner)
+                {
                     printf("Voici, c'est le bon nombre !\n\n ");
-
+                }
                 else if(nombre > nombreADeviner)
+                {
                     printf("Non, il faut chercher un nombre plus petit \n");
-
+                }
                 else if (nombre < nombreADeviner)  // ---> J ai choisi else if au lieu de else pour garder la condition exclusive propre à celle-ci parmis toutes les éventualités.
-                    printf("Ce n est pas ca il faut chercher un nombre plus grand \n");
-
-
-                // nombreDEssai += essai ;
-                // printf("Vous avez joue en %d ", &nombreDEssai);
-
-
+                {
+                        printf("Ce n est pas ca il faut chercher un nombre plus grand \n");
+                }
             }while(nombre != nombreADeviner);
         }
+        else if(soloOuDuo == 4)
+        {
+            int joueur1 = 0;
+            int joueur2 = 0;
+            printf("Joueur 1, veuillez introduire un nombre à faire deviner à votre voisin");
+            scanf("%d", &joueur1);
+            printf("Joueur 2, veuillez introduire un nombre à faire deviner à votre voisin");
+            scanf("%d", &joueur2);
+        }
+        else
+        {
+            return 0;
+        }
+
         printf("Voilà, vous avez joue en %d coups \n\n ", nombreDEssai);
 
         printf("Recommencer la partie ? \n ");
-        printf("0 = Non merci \n ");
-        printf("1 = Ouiii \n ");
+        printf("5 = non merci \n ");
+        printf("6 = ouiii \n ");
         scanf("%d", &continuerPartie);
 
-        // else if(soloOuDuo == B)
-
-
-    }while(continuerPartie == 1);
-
+    }while(continuerPartie == 6);
 
     return 0;
 }
