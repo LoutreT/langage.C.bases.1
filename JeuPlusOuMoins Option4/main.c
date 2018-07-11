4ca8132aa4eb3568c9441d900656d84e196dbbde
Vincent <v.nassaux@nootos.com>
2018-07-11 02:22:34 +0200

    Améliorations

--- JeuPlusOuMoins Option4/main.c
+++ JeuPlusOuMoins Option4/main.c
@@ -5,9 +5,11 @@
 int main(int argc, char *argv[])
 {
     const int MIN = 1;
-    int MAX = 0;  // Ici j avais oublié de supprimer la constante pour modifier le choix de level entre 100, 1000 et 10000 !
+    int MAX = 0;  // Ici j avais oublié de supprimer la constante pour
+    // modifier le choix de level entre 100, 1000 et 10000 !
 
-    int nombreADeviner = 0;  // Ici j'ai choisi de séparer mes 'int' en plusieurs lignes séparées
+    int nombreADeviner = 0;  // Ici j'ai choisi de séparer mes 'int' en
+    // plusieurs lignes séparées
     int nombre = 0;
     int nombreDEssai = 0;
     int continuerPartie = 0;
@@ -17,11 +19,16 @@ int main(int argc, char *argv[])
 
     srand(time(NULL));
 
-    printf(" Hello ! Voici le jeu du nom 'Plus Ou Moins'\n\n   La regle du jeu est de mettre un \n   nombre jusqu a ce que vous trouviez le bon nombre cache\n");
+    printf(" Hello ! Voici le jeu du nom 'Plus Ou Moins'\n\n");
+    printf("La regle du jeu est de mettre un \n");
+    printf("nombre jusqu a ce que vous trouviez le bon nombre cache\n");
 
     do
     {
-        printf("\n Comment desirez vous le jouer ? \n   Vous souhaiteriez jouer en solo ou bien vous \n   etes deux personnes qui souhaiteraient faire deviner \n   entre elles le bon nombre a deviner ?\n");
+        printf("\n Comment desirez vous le jouer ? \n");
+        printf("Vous souhaiteriez jouer en solo ou bien vous \n");
+        printf("etes deux personnes qui souhaiteraient faire deviner \n");
+        printf("entre elles le bon nombre a deviner ?\n");
         printf("\n  choisissez en tapant 3 ou 4");
         printf("\n  3 = En solo");
         printf("\n  4 = A deux \n");
@@ -29,7 +36,10 @@ int main(int argc, char *argv[])
 
         if(soloOuDuo == 3)
         {
-            printf(" A quel LEVEL voulez-vous remporter le challenge \n   Choix Numero 1 pour de 1 a 100 \n   Choix Numero 2 pour de 1 a 1000 \n   Choix Numero 3 pour de 1 a 10000 ?");
+            printf(" A quel LEVEL voulez-vous remporter le challenge \n");
+            printf(" Choix Numero 1 pour de 1 a 100 \n");
+            printf(" Choix Numero 2 pour de 1 a 1000 \n");
+            printf(" Choix Numero 3 pour de 1 a 10000 ?");
             printf("\n Choisissez en tapant 1, 2 ou 3\n");
             scanf("%d", &level);
 
@@ -54,7 +64,8 @@ int main(int argc, char *argv[])
 
 
             nombreADeviner = (rand() % (MAX - MIN + 1)) + MIN;
-            // printf(nombreADeviner);  // ---> Pour un test, à cause de cette ligne, mon jeu ne fonctionnait plus.
+            // printf(nombreADeviner);  // ---> Pour un test, à cause de
+            // cette ligne, mon jeu ne fonctionnait plus.
 
             do
             {
@@ -69,7 +80,9 @@ int main(int argc, char *argv[])
                 {
                     printf(" Non, il faut chercher un nombre plus petit \n");
                 }
-                else if (nombre < nombreADeviner)  // ---> J ai choisi else if au lieu de else pour garder la condition exclusive propre à celle-ci parmis toutes les éventualités.
+                else if (nombre < nombreADeviner)  // ---> J ai choisi else if au
+                //lieu de "else" pour garder la condition exclusive propre
+                //à celle-ci parmis toutes les éventualités.
                 {
                         printf(" Ce n est pas ca il faut chercher un nombre plus grand \n");
                 }
@@ -82,11 +95,13 @@ int main(int argc, char *argv[])
             int devinette1 = 0 ;
             int devinette2 = 0 ;
 
-            printf(" Joueur 1, votre pseudo sera Dracula, veuillez introduire discretement un nombre a faire deviner a votre voisin et appuyer sur ENTER");
+            printf(" Joueur 1, votre pseudo sera Dracula, veuillez introduire");
+            printf(" discretement un nombre a faire deviner a votre voisin et appuyer sur ENTER");
             scanf("%d", &joueur1);
             printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
-            printf("Joueur 2, votre pseudo sera T-Rex, veuillez introduire discretement un nombre a faire deviner a votre voisin et appuyer sur ENTER");
+            printf("Joueur 2, votre pseudo sera T-Rex, veuillez introduire");
+            printf(" discretement un nombre a faire deviner a votre voisin et appuyer sur ENTER");
             scanf("%d", &joueur2);
             printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
@@ -103,7 +118,8 @@ int main(int argc, char *argv[])
                 }
                 else if(devinette2 > joueur1)
                 {
-                    printf("\n  Ce n'est pas ca, cherchez un nombre plus petit sinon pas de viandes, cela dit y a du bon poisson frais au frigo\n");
+                    printf("\n  Ce n'est pas ca, cherchez un nombre plus petit sinon pas ");
+                    printf("de viandes, cela dit y a du bon poisson frais au frigo\n");
                 }
                 else if(devinette2 < joueur1)
                 {
@@ -116,7 +132,8 @@ int main(int argc, char *argv[])
 
                 if(devinette1 == joueur2)
                 {
-                    printf("\n  Dracula, vous avez gagner, vous avez eu T-Rex par derriere, mais bonne chance quand meme avec cette bebete\n");
+                    printf("\n  Dracula, vous avez gagner, vous avez eu T-Rex par derriere,");
+                    printf(" mais bonne chance quand meme avec cette bebete\n");
                 }
                 else if(devinette1 > joueur2)
                 {
@@ -124,7 +141,8 @@ int main(int argc, char *argv[])
                 }
                 else if(devinette1 < joueur2)
                 {
-                    printf("\n  mmmh, cherchez un nombre plus grand y a du sot-l-y-laisse par la, miam miam \n");
+                    printf("\n  mmmh, cherchez un nombre plus grand y a");
+                    printf(" du sot-l-y-laisse par la, miam miam \n");
                 }
 
             }while(devinette2 != joueur1 && devinette1 != joueur2);
@@ -133,7 +151,7 @@ int main(int argc, char *argv[])
         else
         {
             printf("Vous semblez ne pas avoir choisi 3 ou 4, donc abandon.");
-            return ;
+            return 0;
         }
 
         printf("\n\n Voila, vous avez joue comme des saigneurs en %d coups \n\n ", nombreDEssai);
